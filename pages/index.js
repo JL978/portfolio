6c90c424dd9f53949105ac58cf44ec55cb2bdc93
Jimmy Lam <JimmyLam978@gmail.com>
2020-12-30 17:03:52 -0500

    about page done

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,37 @@ export default function Home() {
 				</div>
 			</div>
 			<div className="page">
+				<div className="about">
+					<div className="info">
+						<h3>about me</h3>
+						<h2>Can I really make web apps out of nanoparticles?</h2>
+						<p>Not really no, sorry for the clickbait title :)</p>
+						<p>
+							Although I did go to the University of Toronto for 5 years to be a
+							materials engineer. That includes an year-long internship where I
+							was making{" "}
+							<b>nanoparticles that turn carbon dioxide into renewable fuels</b>
+						</p>
+						<p>
+							That said, I discovered my love for programming during the very
+							same internship by building <b>my first program</b> and I now
+							dedicate my time self-learning and practicing all things web
+							development
+						</p>
+						<p>
+							I believe project-based learning is the fastest way to learn,
+							therefore I have wasted no time building apps using techonolgies
+							such as React, Node.js, Angular, Express.js, Socket.io, etc.
+						</p>
+						<a href="#projects">
+							<b>Check out my projects</b>
+						</a>
+					</div>
+					<div className="img"></div>
+				</div>
+			</div>
+
+			<div className="page">
 				<div className="contact">
 					<div className="info">
 						<h2>Contacts</h2>
